7e46a70687b6a168a2c6f070954e14d75bf6ef2e
Fadhli Nur Himawan <fadhlinurhimawan@gmail.com>
2025-04-08 08:38:53 +0700

    feat: add localization on top10.tsx

--- src/app/home/components/top10.tsx
+++ src/app/home/components/top10.tsx
@@ -1,39 +1,54 @@
-"use client"
+'use client';
 
-import React from "react";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
-import { Card2 } from "@/components/Cards";
-import useHome from "@/app/home/hooks";
+import React from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Card2 } from '@/components/Cards';
+import useHome from '@/app/home/hooks';
+import { movie } from '@/types/movie';
 
-export default function Top10 () {
+export default function Top10() {
+  const { t, top10: data, scrollWrapperRef, scroll } = useHome();
 
-    const { top10: data, scrollWrapperRef, scroll } = useHome();
-
-    return (
-        <>
-            {/** TOP 10 */}
-            <div className="w-full h-auto pb-5 rounded-b-xl drop-shadow-xl border-b border-black bg-white">
-            <div className="w-10/12 m-auto flex flex-col items-start gap-5">
-                <div className="w-full flex justify-between">
-                    <button className="flex gap-2 b3-button">Top 10 Recomendation</button>
-                    <div className="w-fit flex gap-2 justify-between">
-                        <button className='b3-button cursor-pointer' onClick={() => scroll("left")}><FiChevronLeft /></button>
-                        <button className='b3-button dcursor-pointer' onClick={() => scroll("right")}><FiChevronRight /> </button>
-                    </div>
-                </div>
-                <div className="w-full flex gap-8 overflow-x-scroll snap-x" ref={scrollWrapperRef}>
-                {data.map((item: any, index: number) => (
-                            <Card2 
-                                key={index}
-                                title={item.Title}
-                                desc={item.Plot}
-                                poster={item.Poster}
-                                url={item.URL}
-                            />
-                    ))}
-                </div>
+  return (
+    <>
+      {/** TOP 10 */}
+      <div className="w-full h-auto pb-5 rounded-b-xl drop-shadow-xl border-b border-black bg-white">
+        <div className="w-10/12 m-auto flex flex-col items-start gap-5">
+          <div className="w-full flex justify-between">
+            <button className="flex gap-2 b3-button">
+              {t('top10Recommendations')}
+            </button>
+            <div className="w-fit flex gap-2 justify-between">
+              <button
+                className="b3-button cursor-pointer"
+                onClick={() => scroll('left')}
+              >
+                <FiChevronLeft />
+              </button>
+              <button
+                className="b3-button dcursor-pointer"
+                onClick={() => scroll('right')}
+              >
+                <FiChevronRight />{' '}
+              </button>
             </div>
-            </div>
-        </>
-    )
+          </div>
+          <div
+            className="w-full flex gap-8 overflow-x-scroll snap-x"
+            ref={scrollWrapperRef}
+          >
+            {data.map((item: movie, index: number) => (
+              <Card2
+                key={index}
+                title={item.Title}
+                desc={item.Plot}
+                poster={item.Poster}
+                url={item.URL}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
